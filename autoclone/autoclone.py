3a95095efab8eed3f3647887ad42bed7f81fdcf5
Palafita <ppomoura@gmail.com>
2021-03-29 17:17:08 -0300

    updated autoclone script. small change to forkchecker
    now forkchecker outputs are on a folder.

--- autoclone/autoclone.py
+++ autoclone/autoclone.py
@@ -1,16 +1,16 @@
 import os
 import pandas as pd
 
-data = pd.read_csv('enterprise_projects.txt',  sep = '\t', header = None) #Dataset principal, com 29 colunas.
-data.columns = ['url', 'project_id', 'sdtc', 'mcpc', 'mcve', 'star_number', 'commit_count', 'files', 'lines', 'pull_requests', 'github_repo_creation', 'earliest_commit', 'most_recent_commit', 'commiter_count', 'author_count', 'dominant_domain', 'dominant_domain_committer_commits', 'dominant_domain_author_commits', 'dominant_domain_committers', 'cik', 'fg500', 'sec10k', 'sec20f', 'nan', 'project_name', 'owner_login', 'company_name', 'owner_company', 'license']
+forkset = pd.read_csv('datasetfork.txt', sep = '\t', header = None)
+forkset.columns = ['url','owner_login', 'project_name', 'dominant_domain', 'fork']
 
-# first_10 = data.url.head(1000) #O valor é o número de repositórios a serem clonados nesta execução
-# name = data.project_name.head(1000) #msm de cima
+cloneurl = forkset.url
+name = forkset.project_name 
 
-first_10 = data.url
-name = data.project_name 
-#Comentar aqueles de cima e descomentar esse para clonar TODOS os repositórios
+counter = 0
 
-for x, i in zip(first_10, name):
-    # os.system('git clone {} D:\gitrepo\{}'.format(x,i))
-    print('git clone {} D:\gitrepo\{}'.format(x,tamanho)) # para testar output 
+for x, i in zip(cloneurl, name):
+  # os.system('git clone {} D:\gitrepo\{}'.format(x,i)) #the actual cloning line
+  print('git clone {} D:\gitrepo\{}'.format(x,i)) # output testing
+  counter+=1  
+  print("This is the #{} the loop runs!".format(counter))
